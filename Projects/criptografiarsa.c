f00bf99d4e02ed7bb9c9da711d2c7a88f8f6d3a4
Anderson Clemente <anderson.clementte@gmail.com>
2022-03-06 13:20:58 -0300

    I found the final code on trello, making an update

--- Projects/criptografiarsa.c
+++ Projects/criptografiarsa.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+#include <ctype.h>
 long long int inverso(long long int a, long long int m) 
 { 
     a = a%m; 
@@ -43,9 +45,29 @@ long long int mdc(long long int a, long long int b)
     }
 }
 
+int code_decode(long long int M, long long int n, long long int e)
+{
+   long long int c=1;
+    while(e>0)
+    {
+        //com base no peq. teo. de fermat, fazemos a exponenciação modular
+        if(e % 2 ==1)
+        {
+            c=(c * M)%n;
+        }
+        M=(M*M)%n;
+        e=e/2;
+    }
+    return c;
+  
+}
+
 int main()
 {
     long long int escolha, p, q, fi, n, e, d, i;
+  
+  	char mensagem[1000], alfabeto[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+      
     num_invalido:
     printf("Olá, esse é um programa para codificar e descodificar mensagens utilizando criptografia RSA\nEscolha uma das opções\n1:Gerar Chave publica e privada\n2:criptografar\n3:descriptografar\n");
     scanf("%lld", &escolha);
@@ -62,7 +84,7 @@ int main()
         n=p*q;//Primeira chave publica
         fi=(p-1)*(q-1);//so pra conta (anderson)
         expoente_invalido:
-        printf("Valor de fi: %lld\nDigite um valor para o expoente(entre 1 e %lld e mdc de fi expoente eh=1)\n", fi, fi);
+        printf("Valor de φ: %lld\nDigite um valor para o expoente(entre 1 e %lld e mdc de φ expoente eh=1)\n", fi, fi);
         scanf("%lld", &e);
         if(mdc(e, fi)==1)
         {   
@@ -75,15 +97,100 @@ int main()
         }
         // "e" eh a segunda chave publica
         d=inverso(e, fi); //ultima chave privada
-        printf("Chaves publicas (%lld %lld)\nChaves privadas (%lld %lld %lld)\n", n, e, p, q, d);
-
+        printf("Chaves pu viblicas (%lld %lld)\nChaves privadas (%lld %lld %lld)\n", n, e, p, q, d);
+        escolhaerrada:
+        printf("Escolha o proximo passo.\n1: Fechar o programa\n2: Codificar a mensagem\n3: decodificar mensagem\n");
+        scanf("%lld", &escolha);
+        if(escolha==1)
+        {
+            goto final;
+        }
+        else if(escolha==2)
+        {
+            goto p2;
+        }
+        else if(escolha==3)
+        {
+            goto p3;
+        }
+        else
+        {
+            goto escolhaerrada;
+        }
+        
     }
     else if(escolha==2)
-    {
+    {   
+        long long int vetor[100], aux;
+        p2:
+				printf("insira as chaves publicas(na ordem gerada)\n");
+        scanf("%lld%lld", &n, &e);
+        char lixo;
+        scanf("%c", &lixo);
+        printf("Digite uma mensagem para ser criptografada.\n");
+        scanf("%[^\n]", mensagem);
+      	for(i=0; i<strlen(mensagem);i++)
+        {
+          mensagem[i]=toupper(mensagem[i]);
+          for(aux = 0; aux<27; aux++)
+          {
+            if(mensagem[i]==alfabeto[aux])
+            {
+                vetor[i]=aux;
+                printf("%d ", code_decode(vetor[i], n, e));
+            }
+          }
+          
+        }
 
+        printf("\n");
+        escolhaerrada2:
+        printf("Escolha o proximo passo.\n1: Fechar o programa\n2: decodificar mensagem.\n");
+        scanf("%lld", &escolha);
+         if(escolha==1)
+        {
+            goto final;
+        }
+        else if(escolha==2)
+        {
+            goto p3;
+        }
+        else
+        {
+            goto escolhaerrada2;
+        }
+        
+
+        
     }
     else if(escolha==3)
     {
+		long long int vetor[100], aux;
+        p3:
+		printf("insira as 3 chaves privadas (na ordem gerada)\n");
+        scanf("%lld%lld%lld", &p, &q, &d);
+        n=p*q;
+        char lixo;
+        scanf("%c", &lixo);
+        printf("Digite uma mensagem para ser descriptografada.\n");
+      	for(i=0; i<strlen(mensagem);i++)
+        {
+          scanf("%lld", &vetor[i]);
+          vetor[i]=code_decode(vetor[i], n, d);
+          for(aux = 0; aux<27; aux++)
+          {
+            if(vetor[i]==aux)
+            {
+                
+                printf("%c", alfabeto[aux]);
+            }
+            
+          }
+
+        }
+        printf("\n");
+        goto final;
+        
 
     }
     else
@@ -91,4 +198,7 @@ int main()
         printf("Escolha invalida\n\n");
         goto num_invalido;
     }
+    final:
+    return 0;
+
 }
